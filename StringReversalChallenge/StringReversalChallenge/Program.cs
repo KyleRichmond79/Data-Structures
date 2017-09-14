e79de9433f3c9ea343a640045f6e20a14fa6cabe
Kyle Richmond <kylerichmond79@gmail.com>
2017-09-13 19:50:30 -0700

    Refactored StringReversalChallenge to include longer method. Updated README.md

--- StringReversalChallenge/StringReversalChallenge/Program.cs
+++ StringReversalChallenge/StringReversalChallenge/Program.cs
@@ -5,18 +5,32 @@ namespace StringReversalChallenge
 {
     class Program
     {
+        //This is the method that is the entry point for the Program to execute.
+        //It contains a console write line asking for a string that will be reversed and calls
+        //the string reversal method passing the input of the user.
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a string that will be reversed:");
             StringReversal(Console.ReadLine());
         }
 
+        //static void StringReversal(string entry)
+        //{
+        //    Console.WriteLine(new string(entry.ToCharArray().Reverse().ToArray()));
+        //    Console.Read();
+        //}
+
+        //This is a the String Reversal method that handles the logic for reversing the string
+        //that has been inputed by the user.
         static void StringReversal(string entry)
         {
-            Console.WriteLine(new string(entry.ToCharArray().Reverse().ToArray()));
+            string output = "";
+            for (int i = (entry.Length - 1); i> -1; i--)
+                {
+                    output += entry[i];
+                }
+            Console.WriteLine(output);
             Console.Read();
         }
-
-
     }
 }
