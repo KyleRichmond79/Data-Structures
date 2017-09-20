b512fe92251cd491d3b4194db45de7b5c0a02d23
Kyle Richmond <kylerichmond79@gmail.com>
2017-09-19 18:26:27 -0700

    Completed FindMiddleNode with the Math.floor helper method. I do know and understand using a Runner and a Walker to find the middle but did not refactor to implement this knowledge in the solution.

---
+++ LinkedLists/FindMiddle/FindMiddleNode/FindMiddleNode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindMiddleNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList potatoList = new LinkedList(1);
+            potatoList.AddNode(2);
+            potatoList.AddNode(19);
+            potatoList.AddNode(38);
+            potatoList.AddNode(63);
+            potatoList.AddNode(15);
+            potatoList.AddNode(69);
+            potatoList.AddNode(85);
+            potatoList.AddNode(33);
+            potatoList.AddNode(42);
+            potatoList.AddNode(27);
+            potatoList.AddNode(6);
+            potatoList.AddNode(50);
+            potatoList.AddNode(99);
+            potatoList.AddToFirst(0);
+            potatoList.ViewLinkedList();
+            potatoList.FindCenterNode();
+            Console.Read();
+        }
+    }
+}
