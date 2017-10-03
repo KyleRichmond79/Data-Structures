b916feb5f437958d83121449cb5d6f05387a3823
Kyle Richmond <kylerichmond79@gmail.com>
2017-10-02 20:18:57 -0700

    commit to save changes

---
+++ StacksQueueKyle/StacksQueueKyle/Queue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksQueueKyle
+{
+    class Queue : LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public Queue(int data)
+        {
+            Head = new Node(data);
+            Tail = Head;
+        }
+        public void AddQ(int data)
+        {
+            Tail.Next = new Node(data);
+        }
+    }
+}
---
+++ StacksQueueKyle/StacksQueueKyle/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksQueueKyle
+{
+    class Stack : LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public Stack(int data)
+        {
+            Head = new Node(data);
+            Tail = Head;
+        }
+        public void Push(int data)
+        {
+            Node temp = Head;
+            Head = new Node(data)
+            {
+                Next = temp
+            };
+        }
+        public void Pop(int data)
+        {
+            Node temp = Tail;
+            
+        }
+    }
+}
